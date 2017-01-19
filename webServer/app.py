708b4339bfb3723438de451a0a199d7e930f9acb
Appler <e1025658@student.tuwien.ac.at>
2017-01-19 15:57:42 +0100

    to debug

--- webServer/app.py
+++ webServer/app.py
@@ -12,8 +12,7 @@ def index():
 def publish_hello():
 	consumerConfig = {'bootstrap.servers': 'localhost', 'group.id': 'output', 'session.timeout.ms': 6000, 'default.topic.config': {'auto.offset.reset': 'smallest'}}
 	consumer = Consumer(**consumerConfig)
-	#topic = 'BoltOutput'
-	topic = ['taxilocs']
+	topic = ['BoltOutput']
 	consumer.subscribe(topic)
 
 	def events():
